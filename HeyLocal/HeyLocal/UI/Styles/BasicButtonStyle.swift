b4ed6c731fbceb202340ea96a0a514b58a1b139b
choijungp <wjddls980912@naver.com>
2022-08-23 20:36:43 +0900

    S3T-375-Feat: 회원가입 UI 구현

---
+++ HeyLocal/HeyLocal/UI/Styles/BasicButtonStyle.swift
@@ -0,0 +1,19 @@
+//
+//  BasicButtonStyle.swift
+//  HeyLocal
+//
+//  Copyright (c) 2022 TGT All rights reserved.
+//
+
+import SwiftUI
+
+// Basic Button Style 구조체 : 로그인/회원가입/인증/중복확인, 이전/다음
+struct BasicButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 80, height: 40)
+            .font(.system(size: 16))
+            .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
+            .cornerRadius(6.0)
+    }
+}
