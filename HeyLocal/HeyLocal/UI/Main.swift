7aacbc316bd983818156cb4ac3adad412255c2a7
Woody <gintooooonic@gmail.com>
2022-08-23 19:16:50 +0900

    S3T-380-Feat: 하단 탭 구현
    - 5개의 하단 탭을 구현하였습니다.
    - 잘못된 주석을 수정하였습니다. (ContentView.swift -> Main.swift)

--- HeyLocal/HeyLocal/UI/Main.swift
+++ HeyLocal/HeyLocal/UI/Main.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  Main.swift
 //  HeyLocal
 //
 //  Copyright (c) 2022 TGT All rights reserved.
@@ -7,10 +7,66 @@
 
 import SwiftUI
 
+/// `TabView`를 보여주기 위한 뷰입니다.
 struct Main: View {
+	@State private var tabSelection = Tab.home
+	
 	var body: some View {
-		Text("Hello, world!")
-			.padding()
+		TabView(selection: $tabSelection) {
+			// 홈 탭
+			HomeScreen()
+				.tag(Tab.home)
+				.tabItem {
+					Image(systemName: "house.fill")
+					Text("홈")
+				}
+			
+			// 여행 On 탭
+			// TODO: 여행 On 화면 뷰로 대체
+			HomeScreen()
+				.tag(Tab.home)
+				.tabItem {
+					Image(systemName: "note.text")
+					Text("여행 On")
+				}
+			
+			// 마이플랜 탭
+			// TODO: 마이플랜 화면 뷰로 대체
+			HomeScreen()
+				.tag(Tab.home)
+				.tabItem {
+					Image(systemName: "suitcase.cart.fill")
+					Text("마이플랜")
+				}
+			
+			// 메시지 탭
+			// TODO: 메시지 화면 뷰로 대체
+			HomeScreen()
+				.tag(Tab.home)
+				.tabItem {
+					Image(systemName: "message.fill")
+					Text("메시지")
+				}
+			
+			// 내 정보 탭
+			// TODO: 내 정보 화면 뷰로 대체
+			HomeScreen()
+				.tag(Tab.home)
+				.tabItem {
+					Image(systemName: "person")
+					Text("내 정보")
+				}
+		}
+	}
+	
+	/// 탭 유형에 대한 열거형입니다.
+	/// `Main` 내부에서만 사용됩니다.
+	private enum Tab {
+		case home
+		case travelOn
+		case myPlan
+		case message
+		case myinfo
 	}
 }
 
