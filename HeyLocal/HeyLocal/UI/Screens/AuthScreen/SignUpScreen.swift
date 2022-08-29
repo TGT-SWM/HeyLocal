904eea0132e464cdebd48675e825dad75e8d0fb5
choijungp <wjddls980912@naver.com>
2022-08-29 10:06:56 +0900

    S3T-373-Feat: 로그인 UI 구현

--- HeyLocal/HeyLocal/UI/Screens/AuthScreen/SignUpScreen.swift
+++ HeyLocal/HeyLocal/UI/Screens/AuthScreen/SignUpScreen.swift
@@ -13,97 +13,93 @@ struct SignUpScreen: View {
     @State private var user_nickname: String = ""
     
     var body: some View {
-        ScrollView {
-            VStack {
-                // Title
-                Text("회원가입")
-                    .font(.title)
-                    .fontWeight(.bold)
-                
-                // ID · PWD · NickName
-                Group {
+        VStack {
+            // Title
+            Text("회원가입")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            // ID · PWD · NickName
+            Group {
+                VStack {
+                    
                     VStack {
-                        
-                        VStack {
-                            Text("아이디")
-                                .fontWeight(.bold)
-                                .frame(maxWidth: ScreenSize.width * 0.9, alignment: .leading)
-                                
-                            
-                            Spacer()
-                                .frame(height: 3)
-                            
-                            HStack{
-                                TextField("", text: $user_id)
-                                    .frame(width: ScreenSize.width * 0.6, height: ScreenSize.height * 0.05)
-                                    .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
-                                
-                                Spacer()
-                                    .frame(width: 10)
-                                
-                                Button(action: {
-                                    
-                                }, label: {
-                                    Text("중복확인")
-                                })
-                                .buttonStyle(BasicButtonStyle())
-                            }
+                        Text("아이디")
+                            .fontWeight(.bold)
                             .frame(maxWidth: ScreenSize.width * 0.9, alignment: .leading)
-                        }
-                        
-                        // PASSWORD
-                        VStack{
-                            Text("비밀번호")
-                                .font(.system(size: 15))
-                                .fontWeight(.bold)
-                                .frame(maxWidth: ScreenSize.width * 0.9, alignment: .leading)
                             
-                            Spacer()
-                                .frame(height: 3)
-                            
-                            HStack{
-                                SecureField("", text: $user_pwd)
-                                    .frame(width: ScreenSize.width * 0.9, height: ScreenSize.height * 0.05)
-                                    .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
-                            }
-                            .frame(maxWidth: ScreenSize.width * 0.9, alignment: .leading)
-                        }
-                        
-                        // NICK NAME
-                        VStack{
-                            Text("닉네임")
-                                .fontWeight(.bold)
-                                .frame(maxWidth: ScreenSize.width * 0.9, alignment: .leading)
-                            
-                            Spacer()
-                                .frame(height: 3)
-                            
-                            HStack{
-                                TextField("", text: $user_nickname)
-                                    .frame(width: ScreenSize.width * 0.9, height: ScreenSize.height * 0.05)
-                                    .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
-                            }
-                        }
                         
                         Spacer()
-                            .frame(height: 30)
+                            .frame(height: 3)
                         
-                        Button(action: {
+                        HStack{
+                            TextField("", text: $user_id)
+                                .frame(width: ScreenSize.width * 0.6, height: ScreenSize.height * 0.05)
+                                .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
                             
-                        }, label: {
-                            Text("회원가입")
-                        })
-                        .buttonStyle(BasicButtonStyle())
-                        
-                        Spacer()
-                            .frame(height: 20)
+                            Spacer()
+                                .frame(width: 10)
+                            
+                            Button(action: {
+                                
+                            }, label: {
+                                Text("중복확인")
+                            })
+                            .buttonStyle(BasicButtonStyle())
+                        }
+                        .frame(maxWidth: ScreenSize.width * 0.9, alignment: .leading)
                     }
+                    
+                    // PASSWORD
+                    VStack{
+                        Text("비밀번호")
+                            .font(.system(size: 15))
+                            .fontWeight(.bold)
+                            .frame(maxWidth: ScreenSize.width * 0.9, alignment: .leading)
+                        
+                        Spacer()
+                            .frame(height: 3)
+                        
+                        HStack{
+                            SecureField("", text: $user_pwd)
+                                .frame(width: ScreenSize.width * 0.9, height: ScreenSize.height * 0.05)
+                                .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
+                        }
+                        .frame(maxWidth: ScreenSize.width * 0.9, alignment: .leading)
+                    }
+                    
+                    // NICK NAME
+                    VStack{
+                        Text("닉네임")
+                            .fontWeight(.bold)
+                            .frame(maxWidth: ScreenSize.width * 0.9, alignment: .leading)
+                        
+                        Spacer()
+                            .frame(height: 3)
+                        
+                        HStack{
+                            TextField("", text: $user_nickname)
+                                .frame(width: ScreenSize.width * 0.9, height: ScreenSize.height * 0.05)
+                                .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
+                        }
+                    }
+                    
+                    Spacer()
+                        .frame(height: 30)
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("회원가입")
+                    })
+                    .buttonStyle(BasicButtonStyle())
+                    
+                    Spacer()
+                        .frame(height: 20)
                 }
-                
-                content
-                
-            } // end of VStack
-        } // end of ScrollView
+            }
+            content
+        } // end of VStack
     }
     
     var content: some View {
