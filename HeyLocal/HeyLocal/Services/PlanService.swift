fa5d3bcde19a82e6b83c3b6bdd1b350e55447496
Woody <gintooooonic@gmail.com>
2022-09-02 00:07:39 +0900

    S3T-394-Feat: PlanService 추가

---
+++ HeyLocal/HeyLocal/Services/PlanService.swift
@@ -0,0 +1,22 @@
+//
+//  PlanService.swift
+//  HeyLocal
+//
+//  Copyright (c) 2022 TGT All rights reserved.
+//
+
+import Foundation
+import Combine
+
+struct PlanService {
+	private let planRepository = PlanRepository()
+	
+	func getMyPlans() -> AnyPublisher<MyPlans, Error> {
+		// KeyChain에서 로그인 정보 가져오기
+		// TODO: 실제 로그인된 userId 가져오도록 수정
+		let userId = 2
+		
+		// API 요청
+		return planRepository.findMyPlans(userId: userId)
+	}
+}
