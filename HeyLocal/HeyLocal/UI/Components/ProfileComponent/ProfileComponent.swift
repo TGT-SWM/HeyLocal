bb84032ee2b05197ba636d5cad7b14c532c51ef1
choijungp <wjddls980912@naver.com>
2022-09-01 17:19:55 +0900

    S3T-407-Fix : Profile Component 분리

---
+++ HeyLocal/HeyLocal/UI/Components/ProfileComponent/ProfileComponent.swift
@@ -0,0 +1,69 @@
+//
+//  ProfileComponent.swift
+//  HeyLocal
+//
+//  Copyright (c) 2022 TGT All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileComponent: View {
+    var body: some View {
+        ZStack {
+            Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255)
+            
+            VStack {
+                Button(action: {}) {
+                    Image(systemName: "gearshape")
+                        .resizable()
+                        .foregroundColor(Color.black)
+                        .frame(width: 15, height: 15)
+                }
+                
+                HStack {
+                    WebImage(url: "https://cdna.artstation.com/p/assets/images/images/034/457/374/large/shin-min-jeong-.jpg?1612345113")
+                        .scaledToFill()
+                        .frame(width: ScreenSize.width * 0.2, height: ScreenSize.width * 0.2)
+                        .clipped()
+                        .cornerRadius(.infinity)
+                    
+                    
+                    
+                    VStack {
+                        HStack {
+                            Text("김현지")
+                                .font(.system(size: 23))
+                                .fontWeight(.bold)
+                            
+                            // 수정버튼
+                            Button(action: {}) {
+                                Image(systemName: "pencil")
+                                    .resizable()
+                                    .foregroundColor(Color.black)
+                                    .frame(width: 15, height: 15)
+                            }
+                        }
+                        
+                        HStack {
+                            Text("내 노하우")
+                            Text("500하우")
+                        }
+                        
+                        HStack {
+                            Text("내 랭킹")
+                            Text("350위")
+                        }
+                        
+                    }
+                }
+            }
+        }
+        .frame(height: ScreenSize.height * 0.2)
+    }
+}
+
+struct ProfileComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileComponent()
+    }
+}
--- HeyLocal/HeyLocal/UI/Screens/ProfileScreen/MyProfileScreen.swift
+++ HeyLocal/HeyLocal/UI/Screens/ProfileScreen/MyProfileScreen.swift
@@ -14,7 +14,7 @@ struct MyProfileScreen: View {
     var body: some View {
         NavigationView {
             VStack {
-                MyProfileComponent()
+                ProfileComponent()
                 
                 GeometryReader { geo in
                     VStack {
@@ -35,61 +35,6 @@ struct MyProfileScreen: View {
             }
         }
 
-    }
-}
-
-struct MyProfileComponent: View {
-    var body: some View {
-        ZStack {
-            Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255)
-            
-            VStack {
-                Button(action: {}) {
-                    Image(systemName: "gearshape")
-                        .resizable()
-                        .foregroundColor(Color.black)
-                        .frame(width: 15, height: 15)
-                }
-                
-                HStack {
-                    WebImage(url: "https://cdna.artstation.com/p/assets/images/images/034/457/374/large/shin-min-jeong-.jpg?1612345113")
-                        .scaledToFill()
-                        .frame(width: ScreenSize.width * 0.2, height: ScreenSize.width * 0.2)
-                        .clipped()
-                        .cornerRadius(.infinity)
-                    
-                    
-                    
-                    VStack {
-                        HStack {
-                            Text("김현지")
-                                .font(.system(size: 23))
-                                .fontWeight(.bold)
-                            
-                            // 수정버튼
-                            Button(action: {}) {
-                                Image(systemName: "pencil")
-                                    .resizable()
-                                    .foregroundColor(Color.black)
-                                    .frame(width: 15, height: 15)
-                            }
-                        }
-                        
-                        HStack {
-                            Text("내 노하우")
-                            Text("500하우")
-                        }
-                        
-                        HStack {
-                            Text("내 랭킹")
-                            Text("350위")
-                        }
-                        
-                    }
-                }
-            }
-        }
-        .frame(height: ScreenSize.height * 0.2)
     }
 }
 
