9ae1c2000a10ea3479a8bcd4c4c2aedfd10db255
Woody <gintooooonic@gmail.com>
2022-09-05 15:03:21 +0900

    S3T-417-Feat: Bridge 헤더 생성
    카카오맵 연동을 위한 Bridge 헤더를 생성합니다.

---
+++ HeyLocal/HeyLocal-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  HeyLocal-Bridging-Header.h
+//  HeyLocal
+//
+//  Copyright (c) 2022 TGT All rights reserved.
+//
+
+#ifndef HeyLocal_Bridging_Header_h
+#define HeyLocal_Bridging_Header_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* HeyLocal_Bridging_Header_h */
