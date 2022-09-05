8a666a410441b412eb330ab339d4832a98015ef0
Woody <gintooooonic@gmail.com>
2022-09-05 15:50:39 +0900

    S3T-417-Feat: 위치 트래킹 Off
    - 플랜 상세 화면에서는 플랜의 장소들을 보여줘야 하므로, 위치 트래킹 Off

--- HeyLocal/HeyLocal/UI/Components/KakaoMap/KakaoMap.swift
+++ HeyLocal/HeyLocal/UI/Components/KakaoMap/KakaoMap.swift
@@ -13,7 +13,7 @@ struct KakaoMap: UIViewRepresentable {
 		// 참고 : http://susemi99.kr/6131/
 		// TODO: 다른 화면으로 이동해도 메모리 해제가 이루어지지 않을 수 있음. 해당 문제 확인하여 수정 필요.
 		let view = MTMapView(frame: .zero)
-		view.currentLocationTrackingMode = .onWithoutHeading
+		view.currentLocationTrackingMode = .off
 		view.showCurrentLocationMarker = true
 		
 		return view
