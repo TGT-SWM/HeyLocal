da4f436ffdede77d879049f215af128b41763711
Woody <gintooooonic@gmail.com>
2022-09-22 00:57:21 +0900

    S3T-447-Feat: NetworkAgent에서 서버 에러를 찾아 throw

--- HeyLocal/HeyLocal/Utils/NetworkAgent.swift
+++ HeyLocal/HeyLocal/Utils/NetworkAgent.swift
@@ -14,9 +14,24 @@ struct NetworkAgent {
 	func run<T: Decodable>(_ request: URLRequest) -> AnyPublisher<T, Error> {
 		return session
 			.dataTaskPublisher(for: request)
-			.map(\.data)
+			.tryMap { data, response in
+				if let httpResp = response as? HTTPURLResponse,
+				   100..<400 ~= httpResp.statusCode {
+					return data
+				} else {
+					let decoder = JSONDecoder()
+					let apiError = try decoder.decode(APIError.self, from: data)
+					throw apiError
+				}
+			}
 			.decode(type: T.self, decoder: JSONDecoder())
 			.receive(on: DispatchQueue.main)
 			.eraseToAnyPublisher()
 	}
+}
+
+/// API에서 반환한 에러를 표현하기 위한 구조체입니다.
+struct APIError: Error, Decodable {
+	var code: String
+	var description: String
 }
