f740a2bcd05f805af0bc220bf3c7685009438731
Woody <gintooooonic@gmail.com>
2022-10-02 12:52:39 +0900

    S3T-469-Feat: 뷰 및 뷰 모델 추가

---
+++ HeyLocal/HeyLocal/UI/Screens/MyPlanScreen/PlanCreateScreen.swift
@@ -0,0 +1,27 @@
+//
+//  PlanCreateScreen.swift
+//  HeyLocal
+//
+//  Copyright (c) 2022 TGT All rights reserved.
+//
+
+import SwiftUI
+
+// MARK: - PlanCreateScreen (플랜 생성 화면)
+
+struct PlanCreateScreen: View {
+	@ObservedObject var viewModel = ViewModel()
+	
+    var body: some View {
+        EmptyView()
+    }
+}
+
+
+// MARK: - Previews
+
+struct PlanCreateScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanCreateScreen()
+    }
+}
---
+++ HeyLocal/HeyLocal/UI/Screens/MyPlanScreen/PlanCreateViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  PlanCreateViewModel.swift
+//  HeyLocal
+//
+//  Copyright (c) 2022 TGT All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: - PlanCreateScreen.ViewModel (뷰 모델)
+
+extension PlanCreateScreen {
+	class ViewModel: ObservableObject {
+		// 의존성
+		private let userService = UserService()
+		
+		// 상태
+		@Published var travelOns = [TravelOn]()
+		
+		// 페이징 관련 상태
+		var lastItemId: Int?
+		let size = 15
+	}
+}
+
+
+extension PlanCreateScreen.ViewModel {
+	func fetchTravelOns() {
+		userService.loadTravelOnsByUser(
+			userId: 2,
+			lastItemId: Binding(
+				get: { self.lastItemId },
+				set: { self.lastItemId = $0 }
+			),
+			size: size,
+			travelOns: Binding(
+				get: { self.travelOns },
+				set: { self.travelOns = $0 }
+			)
+		)
+		
+		
+	}
+}
