09611a9e170d4cc740a9d0aa30da506206a186a9
Woody <gintooooonic@gmail.com>
2022-10-02 12:52:22 +0900

    S3T-469-Feat: UserService 구현

---
+++ HeyLocal/HeyLocal/Services/UserService.swift
@@ -0,0 +1,30 @@
+//
+//  UserService.swift
+//  HeyLocal
+//
+//  Copyright (c) 2022 TGT All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class UserService {
+	let userRepository = UserRepository()
+	
+	var cancellable: AnyCancellable?
+	
+	func loadTravelOnsByUser(userId: Int, lastItemId: Binding<Int?>, size: Int, travelOns: Binding<[TravelOn]>) {
+		cancellable = userRepository.getTravelOnsByUser(
+			userId: userId,
+			lastItemId: lastItemId.wrappedValue,
+			size: size
+		).sink(
+			receiveCompletion: { _ in },
+			receiveValue: {
+				travelOns.wrappedValue.append(contentsOf: $0)
+				lastItemId.wrappedValue = $0.last?.id
+			}
+		)
+	}
+}
